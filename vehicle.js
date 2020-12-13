c99b0ebf7eef5b420c1a686cf8b0b1cff30e6098
jhuiju <jcsa0505@naver.com>
2020-12-13 23:44:32 +0900

    Create vehicle.js

---
+++ vehicle.js
@@ -0,0 +1,21 @@
+function Vehicle() {
+  this.pos = CreateVector();
+  this.vel = createVector();
+  this.acc = createVector();
+}
+
+Vehicle.prototype.update = function() {
+
+}
+
+Vehicle.prototype.show = function() {
+
+}
+
+Vehicle.prototype.arrive = function() { //돌아와!
+
+}
+
+Vehicle.prototype.flee = function() { //도망가!
+
+}
