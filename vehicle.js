8fdd6494971e89807d002928637e38987a0d84fe
jhuiju <jcsa0505@naver.com>
2020-12-14 22:37:36 +0900

    Update vehicle.js

--- vehicle.js
+++ vehicle.js
@@ -2,17 +2,24 @@ function Vehicle(x,y) {
   this.pos = CreateVector(x, y);
   this.vel = createVector(x, y);
   this.acc = createVector();
+  this.target = createVector(x, y);
   this.maxspeed = 30;
   this.maxforce = 20;
   this.r = 10;
 }
 
 Vehicle.prototype.behaviors = function() {
-
+  var arrive = this.arrive(this.target);
+  var flee = this.flee(hand);
+  var hand = createVector(mouseX, mouseY);
+  arrive.mult(1);
+  flee.mult(5);
+  this.applyForce(arrive);
+  this.applyForce(flee);
 }
 
 Vehicle.prototype.flee = function() { //흩어져!
-  
+
 }
 
 Vehicle.prototype.arrive = function() { //돌아와!
